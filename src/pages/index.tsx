6e684be0953269400de6820de4a76425faf0eb09
Joe Pea <trusktr@gmail.com>
2021-11-17 20:42:55 +0000

    add a lume scene, but it does not work yet, LUME needs some updates for latest Solid

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,11 @@
+/* @jsx-import-source lume */
+
 import { createSignal } from "solid-js";
 import Counter from "~/components/Counter";
 import "./index.css";
+import {useDefaultNames} from 'lume'
+
+useDefaultNames()
 
 export default function Home() {
   const [count, setCount] = createSignal(0)
@@ -8,7 +13,13 @@ export default function Home() {
   return (
     <main>
       <h1>Hello 3D world! {count}</h1><br />
-      <Counter onCount={setCount}/>
+      <Counter onCount={setCount} />
+      <div style="width: 300px; height: 400px">
+        <lume-scene webgl>
+          <lume-point-light intensity="0.6" color="white" position="0 0 500"></lume-point-light>
+          <lume-box align-point="0.5 0.5" mount-point="0.5 0.5 0.5" color="deeppink" size="50 50 50"></lume-box>
+        </lume-scene>
+      </div>
       <p>
         Visit{" "}
         <a href="https://solidjs.com" target="_blank">
